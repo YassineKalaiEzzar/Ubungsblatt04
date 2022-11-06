9fb91234592eafaf21b0eac0263cfdfb71cd4a5e
Yassine Kalai Ezzar <116490033+YassineKalaiEzzar@users.noreply.github.com>
2022-11-06 12:17:43 +0100

    Cosinus.java

---
+++ Cosinus.java
@@ -0,0 +1,20 @@
+// //Ubungsblatt04
+// Aufgabe 1: Cosinus
+public class Cosinus {
+    public static void main(String args[]) {
+     int s=0;
+     double x=Math.PI*0.5;
+    for (int i=1;i<x;i++){ 
+        s+=(Math.pow(-1,i)/fac(2*i))*Math.pow(x,2*i);
+      
+        System.out.println(i);}
+      System.out.println("Sum of s="+s);
+    }
+    
+    static int fac(int n){    
+    if (n == 0)    
+        return 1;    
+     else    
+     return(n * fac(n-1));    
+ }     
+}
